0b10741a605809f5216c759cc228b2ab278020b6
valentin <vzaccari33@gmail.com>
2022-04-04 11:08:18 -0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+int main (void) {
+    char juelieta [32];
+    printf ("ingresar nombre: ");
+    scanf ("%s",juelieta);
+    printf ("hola %s\n",juelieta);
+
+    
+   
+
+    return 0;
+}
+
